0a3214258ed2b5033aa17247104013e766760894
Priya Ravi <priyaravi23@gmail.com>
2020-02-25 17:26:55 -0800

    basic styling and added webpack support for roboto font

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+
 import './App.scss';
 
 const App = () => {
     return (
         <div>
-        My React Application
-    </div>
-);
+            My React Application
+            <a href="#" className="button">Open Modal</a>
+        </div>
+    );
 };
 
 export default App;
