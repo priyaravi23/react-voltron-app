95a0920cae951dcc42de0a10087f70adfbfde070
Priya Ravi <priyaravi23@gmail.com>
2020-02-25 16:52:03 -0800

    react application up and running ...

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.querySelector('#my-app'));
+ReactDOM.render(<App />, document.querySelector('#my-react-app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
 module.exports = {
     module: {
         rules: [
@@ -7,7 +9,20 @@ module.exports = {
                 use: {
                     loader: 'babel-loader'
                 }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./static/index.html"
+        })
+    ]
 };
